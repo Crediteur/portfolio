776d61afaa2314a00fd933b750f12dfb68fd223e
Crediteur <tonyyang_2@hotmail.com>
2023-11-10 17:53:18 -0500

    refactor index.astro

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,6 +1,6 @@
 // string paths to images and icons
-const PROJ_PATH = "/public/projects/";
-const ICON_PATH = "/public/icons/";
+const PROJ_PATH = "/projects/";
+const ICON_PATH = "/icons/";
 
 export const projects = [
   {
@@ -8,7 +8,7 @@ export const projects = [
     desc: "A web app that summarizes text using AI.",
     tags: ["React", "Tailwind"],
     url: "https://github.com/Crediteur/react-projects/tree/main/AI-summarizer-web",
-    image: PROJ_PATH + "ai_summarizer.png",
+    image: PROJ_PATH + "ai_summarizer2.png",
   },
   {
     name: "Doted",
@@ -67,26 +67,26 @@ export const skills = [
     items: [
       { name: "TypeScript", icon: ICON_PATH + "typescript.svg" },
       { name: "React", icon: ICON_PATH + "react.svg" },
-      { name: "Svelte", icon: ICON_PATH + "svelte.svg" },
       { name: "Tailwind", icon: ICON_PATH + "tailwindcss.svg" },
+      { name: "C++", icon: ICON_PATH + "cplusplus.svg" },
     ],
   },
   {
     name: "Systems",
     items: [
+      { name: "Python", icon: ICON_PATH + "python.svg" },
       { name: "Node", icon: ICON_PATH + "nodedotjs.svg" },
-      { name: "SQL", icon: ICON_PATH + "sqlite.svg" },
-      { name: "C++", icon: ICON_PATH + "cplusplus.svg" },
+      { name: "MongoDB", icon: ICON_PATH + "mongodb.svg" },
       { name: "C#", icon: ICON_PATH + "csharp.svg" },
     ],
   },
   {
     name: "Data",
     items: [
-      { name: "Python", icon: ICON_PATH + "python.svg" },
+      { name: "JavaScript", icon: ICON_PATH + "javascript.svg" },
       { name: "TensorFlow", icon: ICON_PATH + "tensorflow.svg" },
-      { name: "Pandas", icon: ICON_PATH + "pandas.svg" },
-      { name: "Jupyter", icon: ICON_PATH + "jupyter.svg" },
+      { name: "Svelte", icon: ICON_PATH + "svelte.svg" },
+      { name: "Astro", icon: ICON_PATH + "astro.svg" },
     ],
   },
 ];
@@ -103,8 +103,13 @@ export const links = [
     icon: ICON_PATH + "linkedin.svg",
   },
   {
+    name: "Twitter",
+    url: "",
+    icon: ICON_PATH + "twitter.svg",
+  },
+  {
     name: "Email",
-    url: "tonyyang_2@hotmail.com",
+    url: "mailto:tonyyang_2@hotmail.com",
     icon: ICON_PATH + "gmail.svg",
   },
 ];
